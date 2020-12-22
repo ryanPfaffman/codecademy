4a975614c469433179d2c59d64b44ee33f77d077
ryanPfaffman <73614186+ryanPfaffman@users.noreply.github.com>
2020-12-21 21:36:30 -0500

    Create CaesarCipher.cs

---
+++ CaesarCipher.cs
@@ -0,0 +1,108 @@
+using System;
+
+//I would've just compared to an uppercase char array for speed, but thought the UpperOrLower function was kinda cool to try.
+
+namespace CaesarCipher
+{
+  class Program
+  {
+    static char[] ShiftBack(char[] charArr)
+    {
+      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+      char[] rtnArr = new char[charArr.Length];
+      for (int x = 0; x < charArr.Length; x++)
+      {
+        int indexOf = Array.IndexOf(alphabet, charArr[x]);
+        if (indexOf == -1)
+        {
+          rtnArr[x] = charArr[x];
+        } else 
+        {
+          rtnArr[x] = alphabet[(indexOf - 3) % alphabet.Length];
+        }
+      }
+
+      return rtnArr;
+    }
+
+    static char[] ShiftForward(char[] charArr)
+    {
+      char[] rtnArr = new char[charArr.Length];
+
+      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      
+      int i = 0;
+
+      foreach (char l in charArr)
+      {
+        int indexL = Array.IndexOf(alphabet, l);
+        if (indexL == -1)
+        {
+          rtnArr[i] = charArr[i];
+        } else 
+        {
+          rtnArr[i] = alphabet[(indexL + 3) % alphabet.Length];
+        }
+        i++;
+      }
+      return rtnArr;
+    }
+
+    static int UpperOrLower(char l)
+    {
+      string alphabet = "abcdefghijklmnopqrstuvwxyz";
+      string alphabetUpper = "ABCEDFGHIJKLMNOPQRSTUVWXYZ";
+
+      if (alphabet.Contains(l.ToString()))
+      {
+        return 1;
+      } else if (alphabetUpper.Contains(l.ToString()))
+      {
+        return 2;
+      } else {
+        return 3;
+      }
+    }
+    static void Main(string[] args)
+    {
+      
+      Console.WriteLine("Enter message to encrypt");
+      string msg = Console.ReadLine();
+      int[] lowerOrUpper = new int[msg.Length];
+      int k = 0;
+      foreach (char l in msg)
+      {
+        lowerOrUpper[k] = UpperOrLower(l);
+        k++;
+      }
+
+      msg = msg.ToLower();
+
+      Console.WriteLine(String.Join("",lowerOrUpper));
+
+      char[] firstChange = ShiftForward(msg.ToCharArray());
+
+      for (int x = 0; x < firstChange.Length; x++)
+      {
+        if (lowerOrUpper[x] == 1)
+        {
+          //do nothing
+        } else if (lowerOrUpper[x] == 2)
+        {
+          firstChange[x] = Convert.ToChar(firstChange[x].ToString().ToUpper());
+        } else
+        {
+          //do nothing
+        }
+      }
+
+      string firstChangeS = String.Join("", firstChange);
+
+      Console.WriteLine($"First Change: {firstChangeS}");
+
+      string changeBack = String.Join("", ShiftBack(firstChangeS.ToCharArray()));
+      Console.WriteLine($"Seconds Change: {changeBack}");
+    }
+  }
+}
